54b36db9b791919f416b57a9e36ac1d4e50a1409
gio1971 <giooorgi7@gmail.com>
2022-01-02 23:53:38 +0400

    create repository for edx traning

---
+++ Django/edx1/tasks/views.py
@@ -0,0 +1,34 @@
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django import forms
+from django.urls import reverse
+
+# Create your views here.
+
+
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(max_length=25, label='New task')
+
+
+def index(request):
+    if 'tasks' not in request.session:
+        request.session["tasks"] = []
+
+    return render(request, 'tasks/index.html', {'list':request.session["tasks"]})
+
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        print(form)
+        if form.is_valid():
+            task = form.cleaned_data['task']
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request, 'tasks/add.html', {'form':form})
+
+
+
+    return render(request, 'tasks/add.html', {'form':NewTaskForm()})
