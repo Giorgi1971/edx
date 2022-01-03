7b62ba3cccc76005a8e824012f3723cbda640784
gio1971 <giooorgi7@gmail.com>
2022-01-04 00:49:55 +0400

    added booking models and a href-s

--- Django/airline/flights/models.py
+++ Django/airline/flights/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.fields import CharField
+
 
 # Create your models here.
 
@@ -19,3 +19,11 @@ class Flight(models.Model):
 
     def __str__(self) -> str:
         return f"{self.id}: {self.origin} to {self.destination}"
+
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    flights = models.ManyToManyField(Flight, blank=True, related_name='passengers')
+
+    def __str__(self) -> str:
+        return f"{self.first} {self.last}"
